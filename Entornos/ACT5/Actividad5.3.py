79e99ba4535121257718b35848c5c979a86f3af5
ijasinsky <ijasinsky@nigul.cide.es>
2023-10-30 20:42:22 +0100

    FinAct5.3

--- Entornos/ACT5/Actividad5.3.py
+++ Entornos/ACT5/Actividad5.3.py
@@ -1,7 +1,8 @@
 dia = float(input("Escriba el dia: "))
 mes = float(input("Escriba el mes: "))
 
-if dia <= (1-31):
-print (f"Hoy es: {dia}/{mes}")
+if (dia >= 1 and dia <= 31 and mes >=1 and mes <=12):
+    print (f"Hoy es: {dia} / {mes}")
 else:
-    print(f"Fecha invalida")
+    print ("Fecha invalida")
+
