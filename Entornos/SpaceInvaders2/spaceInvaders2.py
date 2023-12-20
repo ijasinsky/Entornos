e90296ffbea52f54c5f40a66cf5d4cdfc0bd9bbd
ijasinsky <ijasinsky@nigul.cide.es>
2023-12-20 16:01:25 +0100

    SpaceInvaders2

--- Entornos/SpaceInvaders2/spaceInvaders2.py
+++ Entornos/SpaceInvaders2/spaceInvaders2.py
@@ -1,6 +1,6 @@
 import pygame
 import elementos2
-
+import random
 pygame.init()
 
 #creamos pantalla
@@ -14,13 +14,28 @@ FPS = 60
 #booleano de control
 running = True
 
+
+
 posicion = (200,200)
 nave = elementos2.Nave(posicion)
 
  #crear grupo sprites
-grupo_sprites = pygame.sprite.Group(nave)
-grupo_sprites.add(elementos2.Nave((15,100)))
-grupo_sprites.add(elementos2.Nave((400,100)))
+# grupo_sprites = pygame.sprite.Group(nave)
+# grupo_sprites.add(elementos2.Nave((15,100)))
+# grupo_sprites.add(nave)
+# enemigo = elementos2.Enemigo((50,50))
+# grupo_sprites.add(enemigo)
+grupo_sprites_todos = pygame.sprite.Group()
+grupo_sprites_enemigos = pygame.sprite.Group()
+grupo_sprites_balas = pygame.sprite.Group()
+
+grupo_sprites_todos.add(elementos2.Fondo((0,0)))
+grupo_sprites_todos.add(nave)
+
+
+#creacion de enemigos
+ultimo_enemigo_creado = 0
+frequencia_creacion_enemigos = 2000
 
 #bucle principal
 while running:
@@ -31,13 +46,23 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    momento_actual = pygame.time.get_ticks()
+    if(momento_actual > ultimo_enemigo_creado + frequencia_creacion_enemigos):
+        cordX = random.randint(0,pantalla.get_width())
+        cordY = 0
+        enemigo = elementos2.Enemigo(((cordX, cordY)))
+        grupo_sprites_todos.add(enemigo)
+        grupo_sprites_enemigos.add(enemigo)
+        ultimo_enemigo_creado = momento_actual
 
     teclas = pygame.key.get_pressed()
+    # if teclas[pygame.K_LEFT]:
+    #     nave.disparar(grupo_sprites_todos)
     #Fondo
     pantalla.fill((255,255,255))
     #Segundo Sp
-    grupo_sprites.update(teclas)
-    grupo_sprites.draw(pantalla)
+    grupo_sprites_todos.update(teclas, grupo_sprites_todos, grupo_sprites_balas)
+    grupo_sprites_todos.draw(pantalla)
 
     #actualizamos pantalla
     pygame.display.flip()
